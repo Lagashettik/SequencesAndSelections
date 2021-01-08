a0d5a689611f40bc9859dbfc2dcdd4bee8051e44
Lagashettik <Lagashettik@gmail.com>
2021-01-08 19:03:20 +0530

    Error removed

--- UnitConversion.sh
+++ UnitConversion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 inch=42
 oneplotarea=$((60*40))
-converttoFt=`awk 'BEGIN{printf("%101f",'$inch' * 0.083333333)}'`
+converttoFt=`awk 'BEGIN{printf("%1.1f",'$inch' * 0.083333333)}'`
 echo "42in = "$converttoFt "ft"
 converttomtr=`awk 'BEGIN{printf("%0.2f",'$oneplotarea' * 0.3048)}'`
 echo "60ft * 40ft = "$convertmtr"mtr"
